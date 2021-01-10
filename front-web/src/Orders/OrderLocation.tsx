5e146c5b9ae1c1b8c94885ead30c91c0e1d61b54
Carolina Ventura <carolventura1996@gmail.com>
2021-01-10 19:23:55 +0000

    Finish front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,52 +2,57 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
 import { useState } from 'react';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
-    lat: -18.9110558,
-    lng: -48.26201
+  lat: -18.9110558,
+  lng: -48.26201
 }
 
 type Place = {
-    label?: string;
-    value?: string;
-    position: {
-    lat: number;
-    lng: number;
-    };
+  label?: string;
+  value?: string;
+  position: {
+  lat: number;
+  lng: number;
+  };
+}
+
+type Props = {
+  onChangeLocation: (location: OrderLocationData) => void;
 }
 
 
-function OrderLocation() {
+function OrderLocation({ onChangeLocation }: Props) {
 
-    const [address, setAddress] = useState<Place>({
-        position: initialPosition
-    });
+  const [address, setAddress] = useState<Place>({
+     position: initialPosition
+  });
 
-    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
-        const response = await fetchLocalMapBox(inputValue);
+  const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+    const response = await fetchLocalMapBox(inputValue);
       
-        const places = response.data.features.map((item: any) => {
-          return ({
-            label: item.place_name,
-            value: item.place_name,
-            position: {
-              lat: item.center[1],
-              lng: item.center[0]
-            },
-          });
+      const places = response.data.features.map((item: any) => {
+        return ({
+          label: item.place_name,
+          value: item.place_name,
+          position: {
+          lat: item.center[1],
+          lng: item.center[0]
+          },
         });
+      });
       
-        callback(places);
+      callback(places);
       };
       
       const handleChangeSelect = (place: Place) => {
         setAddress(place);
-        //onChangeLocation({
-        //latitude: place.position.lat,
-        //longitude: place.position.lng,
-        //address: place.label!
-        //});
+        onChangeLocation({
+        latitude: place.position.lat,
+        longitude: place.position.lng,
+        address: place.label!
+        });
       };
 
     return (
